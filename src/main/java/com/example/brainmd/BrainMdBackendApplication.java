3282a88e8272c8542d9efa1a136411c7debc70bd
JoelF <joelfaldin@gmail.com>
2025-08-13 16:45:36 -0400

    feat: adding initial Spring Boot project

---
+++ src/main/java/com/example/brainmd/BrainMdBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.brainmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainMdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainMdBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/brainmd/BrainMdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brainmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainMdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
